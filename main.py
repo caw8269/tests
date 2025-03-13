e1f9db797879849f8386726cc3a9b53f5e1ed92f
caw8269 <121042320+caw8269@users.noreply.github.com>
2025-03-13 08:22:47 -0500

    edited files

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import settings
+import tkinter
 from turtle import *
 
 def hilbert(level,angle,step):
     if level == 0:
         return
+    right(angle)
+
+def main():
+    level = int(input())
+    size = 200
+    penup()
+    goto(-size/2.0,size/2.0)
+    pendown()
