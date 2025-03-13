14df50a85520b5ab68b24c1fa380bb7d02b33f68
caw8269 <121042320+caw8269@users.noreply.github.com>
2025-03-13 11:10:58 -0500

    tried to fix issues with tkinter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import settings
 import tkinter
+tkinter.Tk()
 from turtle import *
 
 def hilbert(level,angle,step):
@@ -7,9 +8,13 @@ def hilbert(level,angle,step):
         return
     right(angle)
 
+
 def main():
     level = int(input())
     size = 200
     penup()
     goto(-size/2.0,size/2.0)
     pendown()
+
+if __name__ == "__main__":
+    main()
