0a89dc578344d02675d6cd255cda857b5e24dec1
caw8269 <121042320+caw8269@users.noreply.github.com>
2025-03-13 12:47:29 -0500

    issue with python input command

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
+#import modules and other files that are used
+import random
 import settings
-import tkinter
-tkinter.Tk()
-from turtle import *
 
-def hilbert(level,angle,step):
-    if level == 0:
-        return
-    right(angle)
+#define a secret word from a list
+word_list = ["apple", "banana", "orange","grape", "kiwi"]
+secret_word = random.choice(word_list)
+if settings.debug_Mode:
+    print(secret_word)
 
+#define some variables
+guessed_letters = []
+word_completion = ["_"] * len(secret_word)
 
-def main():
-    level = int(input())
-    size = 200
-    penup()
-    goto(-size/2.0,size/2.0)
-    pendown()
-
-if __name__ == "__main__":
-    main()
+#game loop
+while settings.guesses_left > 0 and "_" in word_completion:
+    #display the current state of the word
+    print(" ".join(word_completion))
+    #get the players guess
+    guess = input("Guess a letter: ").lower()
+    #check if the guess is valid
+    if len(guess) !=1 or not guess.isalpha():
+        print("Please enter a single letter")
+        continue
+    #if the guess has already been made
+    if guess in guessed_letters:
+        print("you already guessed that letter")
+        continue
+    #update guessd letters
+    guessed_letters.append(guess)
--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 #file is used to easily edit variables that can be kept apart from the main script
+guesses_left = 6
 debug_Mode = True
 if (debug_Mode):
     print("settings succesfully imported")
-    
